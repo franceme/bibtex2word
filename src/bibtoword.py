b329755c513094b84da6f9802762298c1e83d958
franceme <frantzme@vt.edu>
2022-06-20 21:35:39 +0000

    Repatched

--- src/bibtoword.py
+++ src/bibtoword.py
@@ -4,34 +4,12 @@ import os,sys
 from optparse import OptionParser
 import xml.etree.cElementTree as ET
 
-<<<<<<< HEAD:src/bibtoword.py
-def create(bibtexfile, xmlfile, append:bool=False, debug:bool=False):
+def create(bibtexfile, xmlfile):
     ktr = 0
     def msg(msg:str, num:int=ktr, self_debug:bool=debug):
         if self_debug:
             print(f"{num} :> {msg}")
 
-    try:
-        parser = bibtex.Parser()
-        bibdata = parser.parse_file(bibtexfile)
-    except NameError:
-        msg('Need an input filename. See --help')
-        sys.exit(1)
-
-    if append:
-        try:
-            ET.register_namespace('', "http://schemas.microsoft.com/office/word/2004/10/bibliography")
-            ET.register_namespace('b', "http://schemas.microsoft.com/office/word/2004/10/bibliography")
-            root = ET.parse(append).getroot()
-        except TypeError:
-            root = ET.Element('b:Sources', {'xmlns:b': "http://schemas.microsoft.com/office/word/2004/10/bibliography"""})
-    else:
-        root = ET.Element('b:Sources', {'xmlns:b': "http://schemas.microsoft.com/office/word/2004/10/bibliography"""})
-
-    for key, entry in bibdata.entries.iteritems():
-        msg(f"{key} :> {entry}")
-=======
-def create(bibtexfile, xmlfile):
     from pybtex.database.input import bibtex
     parser = bibtex.Parser()
     try:
@@ -55,15 +33,12 @@ def create(bibtexfile, xmlfile):
     for key, entry in bibdata.entries.items():
         if False: #options.debug:
             print(key)
->>>>>>> ce2c8147f2f425531ae4a779d65730e4f06ee258:src/bibtex2word.py
+
         source = ET.SubElement(root, 'b:Source')
         tag = ET.SubElement(source, 'b:Tag')
         tag.text = key
         b = bibdata.entries[key].fields
-<<<<<<< HEAD:src/bibtoword.py
-=======
 
->>>>>>> ce2c8147f2f425531ae4a779d65730e4f06ee258:src/bibtex2word.py
         srctypes = {'book': 'Book',
                     'article': 'ArticleInAPeriodical',
                     'incollection': 'ArticleInAPeriodical',
@@ -126,28 +101,17 @@ def create(bibtexfile, xmlfile):
 
 def main():
     parser = OptionParser()
-<<<<<<< HEAD:src/bibtoword.py
-    parser.add_option('-a', '--append', dest='inxml', action='store',help='existing filename (e.g. Sources.xml) to append elements to')
-    parser.add_option('-d', '--debug', dest='debug', action='store_true', default=False, help='debug (useful for broken .bib entries)')
-    parser.add_option('-i', '--input', dest='bibtexfile', type='string', help='input bibtex filename', action='store')
-    parser.add_option('-o', '--output', dest='xmlfile', type='string',default=sys.stdout,help='output filename', action='store')
-=======
+
     #parser.add_option('-a', '--append', dest='inxml', action='store',help='existing filename (e.g. Sources.xml) to append elements to')
     #parser.add_option('-d', '--debug', dest='debug', action='store_true', default=False, help='debug (useful for broken .bib entries)')
     parser.add_option('-i', '--input', dest='bibtexfile', type='string', help='input bibtex filename', action='store')
     parser.add_option('-o', '--output', dest='xmlfile', type='string',default=sys.stdout,help='output filename', action='store')
-
->>>>>>> ce2c8147f2f425531ae4a779d65730e4f06ee258:src/bibtex2word.py
     print('Starting')
     (options, args) = parser.parse_args()
 
     if len(args) > 0:
         print('Warning: extra arguments ignored: ' + ' '.join(args))
-<<<<<<< HEAD:src/bibtoword.py
-    create(options.bibtexfile, options.xmlfile, options.inxml, options.debug)
-=======
     create(options.bibtexfile, options.xmlfile)
->>>>>>> ce2c8147f2f425531ae4a779d65730e4f06ee258:src/bibtex2word.py
 
 if __name__ =='__main__':
     sys.exit(main())
